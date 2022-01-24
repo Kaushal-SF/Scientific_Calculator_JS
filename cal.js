dae7577227ab86e5a3f0cb37c18ddd741b8a2049
Kaushal-SF <kaushal.p@simformsolutions.com>
2022-01-24 18:17:45 +0530

    added more functionality trigo,fun,exp etc/dev

--- cal.js
+++ cal.js
@@ -7,30 +7,83 @@ for (let items of btns) {
     let btn = e.target.innerText;
 
     switch (btn) {
-        case "X":
-            
-            btn = "*";
-            display.innerText += btn; 
-            break;
-
       case "C":
         display.innerText = "";
         break;
 
       case "←":
-          if(display.innerText)
-        display.innerText = display.innerText.slice(0, -1);
+        if (display.innerText)
+          display.innerText = display.innerText.slice(0, -1);
         break;
 
       case "=":
-          try {
+        try {
+          result = display.innerText;
+          display.innerText = eval(result);
+        } catch {
+          //   display.style.color= "red";
+          display.innerText = "Invalid Input";
+        }
+        break;
 
-              result = display.innerText;
-              display.innerText = eval(result);
-          } catch {
-            //   display.style.color= "red";
-              display.innerText = "Invalid Input";
-          }
+      // trigonometry & function start
+
+      case "sin":
+        //   display.innerText += btn;
+        display.innerText = Math.sin(display.innerText);
+        break;
+      case "cos":
+        display.innerText = Math.cos(display.innerText);
+        break;
+      case "tan":
+        display.innerText = Math.tan(display.innerText);
+        break;
+        case "cot":
+          display.innerText = 1 / Math.tan(display.innerText);
+      case "cosec":
+        display.innerText = 1/ Math.sin(display.innerText);
+        break;
+      case "sec":
+        display.innerText = 1 / Math.cos(display.innerText);
+        break;
+
+      case "abs":
+        display.innerText = Math.abs(display.innerText);
+        break;
+      case "round":
+        display.innerText = Math.round(display.innerText);
+        break;
+      case "floor":
+        display.innerText = Math.floor(display.innerText);
+        break;
+      case "ceil":
+        display.innerText = Math.ceil(display.innerText);
+        break;
+
+      // trigonometry & function end
+
+      case "X":
+        btn = "*";
+        display.innerText += btn;
+        break;
+
+      case "÷":
+        btn = "/";
+        display.innerText += btn;
+        break;
+
+      case "π":
+        display.innerText = Math.PI;
+        break;
+      case "e":
+        display.innerText = Math.E;
+        break;
+      case "exp":
+        display.innerText = Math.exp(display.innerText);
+        break;
+      case "mod":
+        btn = "%";
+        display.innerText += btn;
         break;
 
       default:
